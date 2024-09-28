c2988db74e109600c00138dee04d13f8bea15a77
geovko <octobermes@gmail.com>
2024-09-27 21:24:40 -0700

    fixed the errors for displaying the pages

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,24 +5,18 @@ import { useState } from "react";
 function Project(props) {
   const [isHidden, setIsHidden] = useState(true);
 
-  function toggle() {
-    setIsHidden((isHidden = !isHidden));
-
-    const x = document.getElementById("name-card");
-    if (isHidden) {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
-
   const link = `https://geovko.github.io/${props.repo}/`;
   const image = `assets/images/${props.pic}.png`;
+
   return (
     <div className="project-card">
-      <a href={link}>
+      <a
+        onMouseEnter={() => setIsHidden(false)}
+        onMouseLeave={() => setIsHidden(true)}
+        href={link}
+      >
         <img src={image}></img>
-        <div id="name-card" onMouseOver={toggle}>
+        <div style={{ display: isHidden ? "none" : "block" }} id="name-card">
           <h1>{props.title}</h1>
           <p>{props.desc}</p>
         </div>
