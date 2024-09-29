176aa8ea162649de443f4e2f86b7577568532336
geovko <octobermes@gmail.com>
2024-09-28 20:51:15 -0700

    fixed up portfolio page and fleshed out a bit more

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,22 +4,28 @@ function About() {
   return (
     <div className="page">
       <h2>About Me</h2>
-      <img src="" alt="Profile Picture"></img>
-      <div>
-        <br></br>
-        <p>
-          Hello! My name is Philip Kim. I'm currently working part-time at an
-          independent pharmacy as a clerk. I'm also doing minor translating work
-          (from Korean to English) on the side. I have recently completed a
-          Coding Bootcamp provided by UCI and am preparing to further develop
-          myself in the software engineering field.
-        </p>
-        <br></br>
-        <p>
-          I am very curious about many different subjects, and enjoy learning
-          new things. As such, I'm eager to take on new challenges and see what
-          I can learn from them.
-        </p>
+      <div className="content">
+        <img
+          className="profile-image"
+          src="src/assets/images/profile_picture.jpg"
+          alt="Profile Picture"
+        ></img>
+        <div className="profile-content">
+          <br></br>
+          <p>
+            Hello! My name is Philip Kim. I'm currently working part-time at an
+            independent pharmacy as a clerk. I'm also doing minor translating
+            work (from Korean to English) on the side. I have recently completed
+            a Coding Bootcamp provided by UCI and am preparing to further
+            develop myself in the software engineering field.
+          </p>
+          <br></br>
+          <p>
+            I am very curious about many different subjects, and enjoy learning
+            new things. As such, I'm eager to take on new challenges and see
+            what I can learn from them.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,10 +7,12 @@ function Error() {
   return (
     <div className="page" id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <div className="content">
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
     </div>
   );
 }
