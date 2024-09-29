9777aa455963f192be217827c4fb788120c7faed
geovko <octobermes@gmail.com>
2024-09-28 17:45:25 -0700

    adding images

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
+import background from "./assets/images/green-squares-pixels-background.jpg";
 
 function App() {
   return (
     <>
       <Header />
-      <Outlet />
+      <div style={{ backgroundImage: `url(${background})` }}>
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
