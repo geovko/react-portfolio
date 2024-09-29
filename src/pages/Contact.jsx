5cadc5e760435658000d4e37ed5fa4dcfd18d397
geovko <octobermes@gmail.com>
2024-09-28 17:23:02 -0700

    I tried changing it to Email instead of EmailForm

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 // incomplete
-import EmailForm from "../components/EmailForm.jsx";
+import Email from "../components/Email.jsx";
 
 function Contact() {
   return (
@@ -7,7 +7,7 @@ function Contact() {
       <h2>Contact</h2>
       <div className="send-email">
         <h3>Send an email!</h3>
-        <EmailForm />
+        <Email />
       </div>
     </div>
   );
